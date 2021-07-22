8f2012617b145ade7616423484353e581bd22e06
tupiribas <tupyribas@outlook.com>
2021-07-22 15:37:41 -0300

    Foi adicionado a pasta entities e entities.enum, foi criada a classe Order e OrderStatus

--- src/course/Program.java
+++ src/course/Program.java
@@ -1,7 +1,21 @@
 package course;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Olá mundo!");
+		Order order = new Order(123, new Date(), OrderStatus.PROCESSING);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 	}
 }
